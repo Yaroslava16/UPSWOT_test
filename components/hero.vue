48b8c2ce4a7900dfa7e1bfeeadc2a9d5ec3835bd
Yaroslava Musiichuk <iaroslava1607@gmail.com>
2022-03-22 02:08:37 +0200

    make hero

---
+++ components/hero.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="hero">
+    <p class="hero__title">
+      Welcome to<br />
+      Business Analytics Online
+    </p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.hero {
+  display: inline-flex;
+  align-items: end;
+  width: 100%;
+  height: 23.802vw;
+  padding: 0 0 3.646vw 19.323vw;
+  background-image: url(~/assets/hero_img.png);
+  background-repeat: no-repeat;
+  background-size: cover;
+  &__title {
+    width: 32.76vw;
+    font-weight: 400;
+    font-size: 2.865vw;
+    line-height: 156.69%;
+    color: #fff;
+  }
+}
+</style>
--- pages/login.vue
+++ pages/login.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <Header />
+    <Hero />
     <Footer />
   </div>
 </template>
 
 <script>
 import Header from "~/components/header.vue";
+import Hero from "~/components/hero.vue";
 import Footer from "~/components/footer.vue";
 export default {
   components: {
     Header,
+    Hero,
     Footer,
   },
 };
